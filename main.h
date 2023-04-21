7490933f9f0fa175813edac57befd4ab9ee1cdbd
Samson Odunola <odunolasamson@yahoo.com>
2023-04-21 14:13:16 +0000

    Update header

--- main.h
+++ main.h
@@ -1,4 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *format, ...);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
 #endif
