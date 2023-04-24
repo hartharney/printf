d32ef1cd81459e9af89e0e525d1672e593b1df27
Samson Odunola <odunolasamson@yahoo.com>
2023-04-24 21:35:09 +0000

    Add buffer

--- prt_string.c
+++ prt_string.c
@@ -1,4 +1,15 @@
 #include "main.h"
+
+/**
+ * print_buffer - print all the char stored in the buffer
+ * @buffer: pointer to buffer
+ * @num_of_bytes: size of charater stored in buffer
+ * return: void
+ */
+void print_buffer(char *buffer, unsigned int num_of_bytes)
+{
+	write(1, buffer, num_of_bytes);
+}
 /**
  * prt_string - format specifier to print a string
  * @args: variadic argument
@@ -10,9 +21,12 @@ int prt_string(va_list args)
 
 	int i;
 	int count;
+	char buffer[1024];
+	unsigned int num_of_bytes;
 
 	i = 0;
 	count = 0;
+	num_of_bytes = 0;
 
 	if (*s == '\0')
 	{
@@ -20,10 +34,14 @@ int prt_string(va_list args)
 	}
 	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		buffer[i] = s[i];
+		num_of_bytes += 1;
+		/*_putchar(s[i]);*/
 		count += 1;
 		i++;
 	}
+
+	print_buffer(buffer, num_of_bytes);
 
 	return (count);
 }
